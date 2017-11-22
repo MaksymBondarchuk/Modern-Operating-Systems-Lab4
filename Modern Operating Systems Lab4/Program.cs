1ed1e906ad39f27f2e18b5836540ccd7c771e40f
Maksym Bondarchuk <bondarchuk.m.y@gmail.com>
2017-11-22 14:38:11 +0200

    Add project files.

---
+++ Modern Operating Systems Lab4/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modern_Operating_Systems_Lab4
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
